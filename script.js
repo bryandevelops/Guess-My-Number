241d25fa3e81c61e579868ff3eddccf6401f51c2
bryan0147 <bryan0147@gmail.com>
2021-06-08 13:21:58 -0400

    Update gameLogic to account for edge cases

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ let again = document.querySelector(".again");
 let message = document.querySelector(".message");
 let score = document.querySelector(".score");
 let highscore = document.querySelector(".highscore");
+let gameOver = false;
 
 function generateNumber() {
   return Math.floor(Math.random() * 20 + 1);
@@ -18,7 +19,12 @@ function gameLogic(e) {
   if (e.type === "click" || (e.type === "keydown" && e.key === "Enter")) {
     let value = Number(guess.value);
 
-    if (guess.value === "") return;
+    if (guess.value === "" || gameOver === true) return;
+
+    if (value < 0 || value > 20) {
+      message.textContent = "😒 Between 1 and 20";
+      return;
+    }
 
     if (value < answer) {
       message.textContent = "📉 Too low!";
@@ -33,12 +39,14 @@ function gameLogic(e) {
       if (Number(score.textContent) > Number(highscore.textContent)) {
         highscore.textContent = score.textContent;
       }
+      gameOver = true;
     }
 
     if (Number(score.textContent) === 0) {
       background.backgroundColor = "#ad0000";
       number.textContent = answer;
       message.textContent = "😔 You lose";
+      gameOver = true;
     }
   }
 }
@@ -50,6 +58,7 @@ function resetGame() {
   guess.value = "";
   message.textContent = "🤔 Start guessing...";
   score.textContent = "10";
+  gameOver = false;
 }
 
 check.addEventListener("click", gameLogic);
